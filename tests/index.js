109a73301c9c95e612113c13592642686e79cfd4
Josh Johnston <josh@x-team.com>
2016-08-12 09:49:30 +1000

    added tests for createClassname

--- tests/index.js
+++ tests/index.js
@@ -2,9 +2,48 @@ const tape = require('tape')
 const cmz = require('../index.js')
 
 tape('creating tokens', function (t) {
-  t.equals(cmz.tokenFromRelFilename('abc'), 'abc')
-  t.equals(cmz.tokenFromRelFilename('abc/def'), 'abc_def')
-  t.equals(cmz.tokenFromRelFilename('abc/def/ghi.jk'), 'abc_def_ghi')
+  const f = cmz.tokenFromRelFilename
+  t.equals(f('abc'), 'abc')
+  t.equals(f('abc/def'), 'abc_def')
+  t.equals(f('abc/def/ghi.jk'), 'abc_def_ghi')
+
+  t.end()
+})
+
+tape('creating classnames', function (t) {
+  let comps = {}
+  const f = cmz.createClassname
+
+  t.equals(
+    f('abc', comps, 'def'),
+    'abc__def',
+    'Single classname')
+
+  comps = {
+    'a': 'b'
+  }
+  t.equals(
+    f('abc', comps, 'def'),
+    'abc__def',
+    'No matching compositions')
+
+  comps = {
+    'a': 'b',
+    'def': 'ghi'
+  }
+  t.equals(
+    f('abc', comps, 'def'),
+    'abc__def ghi',
+    'One matching composition')
+
+  comps = {
+    'a': 'b',
+    'def': ['ghi', 'jkl']
+  }
+  t.equals(
+    f('abc', comps, 'def'), 
+    'abc__def ghi jkl', 
+    'Multiple matching compositions')
 
   t.end()
 })
