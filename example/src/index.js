24eca1c3bd702b8d13cb78c9e3f99a650e5d42f4
Josh Johnston <josh@x-team.com>
2016-08-16 15:33:14 +1000

    updated example with theme

--- example/src/index.js
+++ example/src/index.js
@@ -5,9 +5,21 @@ const card = renderCard({
 })
 
 const html = `
-<h1>cmz example</h1>
+<div class="screen">
+  <div class="inner">
+    <h2>01. Basic Card</h2>
+    <p>In <a href="https://github.com/joshwnj/cmz/blob/master/example/src/components/card/index.js"><code>src/components/card</code></a> we have a js module with a corresponding css module.</p>
+    <p>When <code>cmz()</code> is called with no arguments, it assumes you want to load the css module that has the same name as the js module you're calling from.</p>
 
-basic card: ${card}
+    ${card}
+  </div>
+</div>
+
+<div class="screen">
+  <div class="inner">
+    <h2>02. Next Example</h2>
+  </div>
+</div>
 `
 
 if (!module.parent) {
