b1345f63961691949d93f2b7c6755187eea16532
Josh Johnston <josh@x-team.com>
2016-08-16 11:44:20 +1000

    added comment about where the css module comes from

--- example/src/components/card/index.js
+++ example/src/components/card/index.js
@@ -1,4 +1,8 @@
 const cmz = require('cmz')
+
+// calling `cmz()` like this, with no arguments,
+// means cmz will reference a css module with the
+// corresponding name to this js module (aka index.css)
 const styles = cmz()
 
 module.exports = function (props) {
