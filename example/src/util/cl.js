ca0b817c2fa0ae29fa97ef77e08951056365f688
Josh Johnston <josh@x-team.com>
2017-03-09 13:19:33 +1100

    cleanup old example files

--- example/src/util/cl.js
+++
@@ -1,9 +0,0 @@
-// shortcut for creating a `class=""` attribute
-// for a css module
-module.exports = function cl (styles) {
-  const map = {}
-  Object.keys(styles).forEach((k) => {
-    map[k] = `class="${styles[k]}"`
-  })
-  return map
-}
