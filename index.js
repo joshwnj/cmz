3b4c3d067314d4f0a5242c78726c9c0327fdaa3d
Josh Johnston <josh@x-team.com>
2017-06-08 15:47:29 +1000

    simplified api

--- index.js
+++ index.js
@@ -10,99 +10,39 @@ function addSemis (raw) {
   return raw.replace(/([^;])\n/g, '$1;\n')
 }
 
-function cmz (a1, a2) {
-  // factory
-  if (typeof a1 === 'function') {
-    return function (data) {
-      const func = a1
-      const name = func.name + '--' + data
-      return cmz(name, func(data))
-    }
+function cmz (name, raw) {
+  if (raw === undefined) {
+    raw = name
+    name = null
   }
-
-  if (!isName(a1)) {
-    return new CmzAtom(createName(), a1)
-  }
-
-  return new (typeof a2 === 'string' ? CmzAtom : CmzMod)(a1, a2)
+  return new Atom(name, raw)
 }
 
-function CmzMod (prefix, raw) {
-  // we'll use the same prefix for all atoms in this family
-  this._prefix = prefix || createName()
-  this._atoms = {}
-  if (raw) { this.add(raw) }
-}
-
-CmzMod.prototype.getAtoms = function () {
-  return this._atoms
-}
-
-CmzMod.prototype.add = function (raw) {
-  const self = this
-  Object.keys(raw).forEach(function (k) {
-    if (raw[k] instanceof CmzAtom) {
-      // families can include pre-created atoms
-      self._addAtom(k, raw[k])
-      return
-    }
-
-    const name = self._prefix + '__' + k
-    if (Array.isArray(raw[k])) {
-      var comps = []
-      var rules = []
-      raw[k].forEach(function (item) {
-        if (item instanceof CmzAtom || isName(item)) {
-          comps.push(item)
-        } else {
-          rules.push(item)
-        }
-      })
-      self._addAtom(k, new CmzAtom(name, rules))
-      if (comps.length) {
-        self[k].compose(comps)
-      }
-    } else {
-      // use the family key to make the classname a bit more descriptive
-      self._addAtom(k, new CmzAtom(name, raw[k]))
-    }
-  })
-  return this
-}
-
-CmzMod.prototype._addAtom = function (key, atom) {
-  // expose atoms directly (but warn if there's a name clash)
-  if (this[key]) {
-    console.warn('[cmz] %s already exists in module %s', key, this._prefix)
-  }
-  this[key] = this._atoms[key] = atom
-}
-
-// compose 2 families together
-CmzMod.prototype.compose = function (other) {
-  const self = this
-  Object.keys(other._atoms).forEach(function (k) {
-    if (self._atoms[k]) {
-      self._atoms[k].compose(other[k])
-    } else {
-      self._addAtom(k, other[k])
-    }
-  })
-
-  return this
-}
-
-function CmzAtom (name, raw) {
+function Atom (name, raw) {
   this.name = name || createName()
-  this.raw = raw
+
+  this.raw = []
   this.comps = []
+  this.compose(raw)
 }
 
-CmzAtom.prototype.hasCss = function () {
-  return this.raw && this.raw.length > 0
+Atom.prototype.compose = function (parts) {
+  if (!Array.isArray(parts)) { parts = [parts] }
+
+  const self = this
+  parts.filter(Boolean).forEach(function (p) {
+    const isComp = isName(p) || p instanceof Atom
+    self[isComp ? 'comps' : 'raw'].push(p)
+  })
+
+  return this
 }
 
-CmzAtom.prototype.getCss = function () {
+Atom.prototype.hasCss = function () {
+  return this.raw && this.raw.length
+}
+
+Atom.prototype.getCss = function () {
   if (!this.hasCss()) { return '' }
 
   const parts = typeof this.raw === 'string' ? [this.raw] : this.raw
@@ -118,7 +58,7 @@ CmzAtom.prototype.getCss = function () {
     part = part.replace(/\?/g, name)
 
     // if no selector placeholder was given, we need to wrap it ourselves
-    const isWrapped = part.indexOf('&') >= 0
+    const isWrapped = part.indexOf('{') >= 0
     const group = isWrapped ? wrapped : unwrapped
     group.push(part)
   })
@@ -139,9 +79,7 @@ CmzAtom.prototype.getCss = function () {
   return output
 }
 
-CmzAtom.prototype.toString = function () {
-  // need to call toString() on the comps first,
-  // so that they appear higher in source
+Atom.prototype.toString = function () {
   const fullName = this.getFullName()
 
   // only need to insert css if we have any
@@ -151,21 +89,14 @@ CmzAtom.prototype.toString = function () {
   return fullName
 }
 
-CmzAtom.prototype.getFullName = function () {
+Atom.prototype.getFullName = function () {
   const comps = this.comps.join(' ')
   return this.hasCss()
     ? this.name + (comps && (' ' + comps))
     : comps
 }
 
-CmzAtom.prototype.compose = function (comps) {
-  if (!Array.isArray(comps)) { comps = [comps] }
-  this.comps = this.comps.concat(comps)
-  return this
-}
-
-cmz.Mod = CmzMod
-cmz.Atom = CmzAtom
+cmz.Atom = Atom
 cmz.reset = createName.reset
 
 module.exports = cmz
--- tests/usage.js
+++ tests/usage.js
@@ -22,49 +22,13 @@ tape('usage', function (t) {
   result = cmz('myAtom', 'color: red')
   t.ok(
     result instanceof cmz.Atom,
-    'cmz/2 creates an Atom when arg 2 looks like a rule'
+    'cmz/2 creates a named Atom when arg 2 looks like a rule'
   )
 
   t.equal(
     result.toString(),
     'myAtom',
     'Named atoms use the name they are given'
-  )
-
-  result = cmz('ModuleName')
-  t.ok(
-    result instanceof cmz.Mod,
-    'cmz/1 creates a Module when arg 1 looks like a name'
-  )
-
-  result = cmz('ModuleName', {
-    class1: 'color: red'
-  })
-  t.ok(
-    result instanceof cmz.Mod,
-    'cmz/2 creates a Module when arg 1 looks like a name'
-  )
-
-  result = cmz('ModuleName', {
-    class1: [
-      'aa',
-      'bb',
-      'color: red'
-    ],
-    class2: [
-      'cc',
-      'dd'
-    ]
-  })
-  t.equal(
-    result.class1.toString(),
-    'ModuleName__class1 aa bb',
-    'Module array syntax: compose names, create a class for rules'
-  )
-  t.equal(
-    result.class2.toString(),
-    'cc dd',
-    'Module array syntax: only create a class if there\'s something to go in it'
   )
 
   t.end()
