d7731f84111af289409063e5e1dac75791f1e81d
Josh Johnston <josh@x-team.com>
2017-06-08 16:22:39 +1000

    simplified api

--- example/src/index.js
+++ example/src/index.js
@@ -1,31 +1,63 @@
-const card = require('./components/card')({
-  image: 'src/assets/doge.png'
-})
+const cmz = require('cmz')
+const attr = require('cmz/attr').string
 
-const customCard = require('./components/custom-card')({
-  image: 'src/assets/doge.png'
-})
+// you can define atoms that define common rules
+const shadow = cmz('shadow', `
+  box-shadow: 1px 1px 5px #000
+`)
+
+// you can also create functions that return atoms
+const BASE_SIZE = 16
+const fontSize = (size) => cmz([
+  // cubic sizing
+  `font-size: ${(size * size * size) + BASE_SIZE}px`,
+
+  // big text will be condensed, smaller text is normal
+  size > 2 && `letter-spacing: -.07em`
+])
+
+// you can create helper functions that construct css,
+// like applying rules when the viewport is wider than a certain width
+const widerThan = (width, css) => cmz(`
+@media screen and (min-width: ${width}px) {
+  & {
+    ${css}
+  }
+}
+`)
+
+// you can create an atom by combining css rules:
+const box = cmz('box', [
+  'width: 500px',
+  'margin: 20px auto',
+  'padding: 20px',
+  widerThan(600, 'color: green'),
+  widerThan(800, 'color: blue'),
+  widerThan(1024, 'color: red'),
+])
+
+// you can also create an atom by composing existing atoms:
+const bigText = cmz([
+  box,
+  fontSize(3)
+])
+
+const medText = cmz([
+  box,
+  fontSize(2)
+])
+
+// or a comnbination of the two
+const smallText = cmz([
+  box,
+  fontSize(1),
+  'font-weight: 800'
+])
 
 const html = `
-<div class="screen">
-  <div class="inner">
-    <h2>01. Basic Card</h2>
-    <p>In <a href="https://github.com/joshwnj/cmz/blob/master/example/src/components/card/index.js"><code>src/components/card</code></a> we have a js module with a corresponding css module.</p>
-    <p>When <code>cmz()</code> is called with no arguments, it assumes you want to load the css module that has the same name as the js module you're calling from.</p>
-
-    ${card}
-  </div>
-</div>
-
-<div class="screen">
-  <div class="inner">
-    <h2>02. Custom Card</h2>
-    <p><a href="https://github.com/joshwnj/cmz/blob/master/example/src/components/custom-card/index.js"><code>src/components/custom-card</code></a> is just like the regular card, except with a few customisations.</p>
-    <p>Rather than using the cascade to redefine base styles, we are composing from the base card classes and layering our own modifications on top. We don't need to create a whole new .css file in this case, since it's only a couple of lines so we inline it in our js component (using <code>cmz.inline(...)</code>).</p>
-    <p>PS. hover the doge for a surprise</p>
-    ${customCard}
-  </div>
-</div>
+  <div ${attr(cmz([bigText, shadow]))}>big condensed text with a shadow</div>
+  <div ${attr(medText)}>medium text</div>
+  <div ${attr(smallText)}>small bold text</div>
 `
 
 if (!module.parent) {
