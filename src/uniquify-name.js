7b672ae5135ca1b28ebc01645723a8aefcf9bd16
Josh Johnston <josh@x-team.com>
2017-09-28 15:01:44 +1000

    don't auto-suffix when a name is provided

--- src/uniquify-name.js
+++ src/uniquify-name.js
@@ -1,15 +1,18 @@
 var _names = { cmz: 0 }
+
 module.exports = function uniquifyName (name) {
-  name = name || 'cmz'
-  var newName = name
-  if (_names[name] !== undefined) {
-    newName += '-' + _names[name]
-    _names[name] += 1
+  if (name) { return name }
+
+  var newName = 'cmz'
+  if (_names.cmz !== undefined) {
+    newName += '-' + _names.cmz
+    _names.cmz += 1
   } else {
-    _names[name] = 1
+    _names.cmz = 1
   }
   return newName
 }
+
 module.exports.reset = function () {
   _names = { cmz: 0 }
 }
